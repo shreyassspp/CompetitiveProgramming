6c2b1b010acc90ada5b9654f060eab43094f7242
shreyassspp <shreyas_pp@ee.iitr.ac.in>
2022-01-27 14:52:18 +0530

    Jan 26

---
+++ CP/1512C.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#define f(i,a,b) for(int i = a; i<b;i++)
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int a,b;
+        cin>>a>>b;
+
+       
+        
+        
+    }
+
+    return 0;
+}
